366d876e2dc9859d12f31887d12c7917884a03ad
Mridul Yadav <mriduly2@gmail.com>
2025-02-26 20:38:40 +0530

    Added User Input

--- testing.java
+++ testing.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class testing{
   public static void main(String [] args){
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter value of n = ");
+    int n = sc.nextInt();
+    System.out.println("Value of n = "+n);
     System.out.println("This is me , Thank you");
   }
 }
